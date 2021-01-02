b3e2b74526b8790e5c27671acd2168eec91db9ed
quifar <risqufaris@gmail.com>
2021-01-02 16:25:25 +0700

    update manage_room.dart : change GridView.builder to StaggeredGridView.builder

--- lib/manage_room.dart
+++ lib/manage_room.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:iot_starbhak_client/detail_room.dart';
 import 'package:page_transition/page_transition.dart';
 
@@ -71,127 +72,145 @@ class ManageRoomState extends State<ManageRoom> {
                       return Form(
                         autovalidateMode: AutovalidateMode.always,
                         child: Container(
-                          child: SingleChildScrollView(
-                            controller: scrollController,
-                            padding: EdgeInsets.all(20),
-                            reverse: true,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      bottom: MediaQuery.of(context)
-                                          .viewInsets
-                                          .top),
+                            child: SingleChildScrollView(
+                          controller: scrollController,
+                          padding: EdgeInsets.all(20),
+                          reverse: true,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    bottom:
+                                        MediaQuery.of(context).viewInsets.top),
+                              ),
+                              Text(
+                                "Add New Room",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 20,
                                 ),
-                                Text(
-                                  "Add New Room",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.w600,
-                                    fontSize: 20,
-                                  ),
+                              ),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              Text(
+                                "Enter Room's Name",
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                SizedBox(
-                                  height: 20,
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                  hintText: "Room's name",
+                                  border: OutlineInputBorder(),
                                 ),
-                                Text(
-                                  "Enter Room's Name",
-                                  style: TextStyle(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.bold,
-                                  ),
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                "Select Room's Icon",
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
                                 ),
-                                SizedBox(height: 8,),
-                                TextFormField(
-                                    decoration: InputDecoration(
-                                      hintText: "Room's name",
-                                      border: OutlineInputBorder(),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              StaggeredGridView.builder(
+                                controller: scrollController,
+                                itemBuilder: (context, index) {
+                                  return Container(
+                                    alignment: Alignment.center,
+                                    padding: EdgeInsets.symmetric(
+                                        vertical: 10, horizontal: 20),
+                                    decoration: BoxDecoration(
+                                      border: Border.all(
+                                        color: Colors.blue,
+                                        style: BorderStyle.solid,
+                                      ),
+                                      borderRadius: BorderRadius.circular(10),
                                     ),
-                                  ),
-                                SizedBox(height: 15,),
-                                Text(
-                                  "Select Room's Icon",
-                                  style: TextStyle(
-                                    color: Colors.blue,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                SizedBox(height: 8,),
-                                GridView.builder(
-                                  controller: scrollController,
-                                  itemBuilder: (context, index) {
-                                    return Container(
-                                      alignment: Alignment.center,
-                                      margin: EdgeInsets.only(right: 10, left: 10, bottom: 10, top: 10),
-                                      padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                                      decoration: BoxDecoration(
-                                        border: Border.all(
+                                    child: Column(
+                                      children: [
+                                        Icon(
+                                          Icons.room_preferences_outlined,
                                           color: Colors.blue,
-                                          style: BorderStyle.solid,
+                                          size: 40,
                                         ),
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                      child: Column(
-                                        children: [
-                                          Expanded(
-                                            child: Icon(
-                                              Icons.room_preferences_outlined,
-                                              color: Colors.blue,
-                                              size: 40,
-                                            ),
-                                          ),
-                                          SizedBox(
-                                            height: 10,
-                                          ),
-                                          Text(
-                                            "Bathroom",
-                                            maxLines: 1,
-                                            softWrap: false,
-                                            overflow: TextOverflow.fade,
-                                            style: TextStyle(fontSize: 14, color: Colors.blue),
-                                          ),
-                                        ],
-                                      ),
-                                    );
-                                  },
-                                  itemCount: 6,
-                                  gridDelegate:
-                                  SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
-                                  shrinkWrap: true,
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          "Bathroom",
+                                          maxLines: 1,
+                                          softWrap: false,
+                                          overflow: TextOverflow.fade,
+                                          style: TextStyle(
+                                              fontSize: 14, color: Colors.blue),
+                                        ),
+                                      ],
+                                    ),
+                                  );
+                                },
+                                itemCount: 6,
+                                gridDelegate:
+                                    SliverStaggeredGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisSpacing: 10,
+                                  mainAxisSpacing: 10,
+                                  crossAxisCount: 3,
+                                  staggeredTileCount: 6,
+                                  staggeredTileBuilder: (index) =>
+                                      StaggeredTile.fit(1),
                                 ),
-                                SizedBox(height: 15,),
-                                Container(
-                                  width: MediaQuery.of(context).size.width,
-                                  child: ElevatedButton(
-                                    style: ButtonStyle(
-                                      padding: MaterialStateProperty.resolveWith((states) => EdgeInsets.all(8)),
-                                      shape: MaterialStateProperty.resolveWith((states) => RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(5),)),
-                                      backgroundColor: MaterialStateProperty.resolveWith((states) => Colors.blue),
-                                    ),
-                                    child: Text(
-                                      "Save",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 22,
-                                      ),
-                                    ),
-                                    onPressed: () {
-                                      Navigator.pop(context);
-                                    },
+                                shrinkWrap: true,
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                child: ElevatedButton(
+                                  style: ButtonStyle(
+                                    padding: MaterialStateProperty.resolveWith(
+                                        (states) => EdgeInsets.all(8)),
+                                    shape: MaterialStateProperty.resolveWith(
+                                        (states) => RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(5),
+                                            )),
+                                    backgroundColor:
+                                        MaterialStateProperty.resolveWith(
+                                            (states) => Colors.blue),
                                   ),
+                                  child: Text(
+                                    "Save",
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 22,
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
                                 ),
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      bottom: MediaQuery.of(context)
-                                          .viewInsets
-                                          .bottom),
-                                ),
-                              ],
-                            ),
-                          )
-                        ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    bottom: MediaQuery.of(context)
+                                        .viewInsets
+                                        .bottom),
+                              ),
+                            ],
+                          ),
+                        )),
                       );
                     },
                   );
@@ -203,18 +222,18 @@ class ManageRoomState extends State<ManageRoom> {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        child: GridView.builder(
+        child: StaggeredGridView.builder(
           itemBuilder: (context, index) {
             return FlatButton(
               padding: EdgeInsets.zero,
-              onPressed: (){
-                Navigator.push(context, PageTransition(child: DetailRoom(), type: PageTransitionType.fade));
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    PageTransition(
+                        child: DetailRoom(), type: PageTransitionType.fade));
               },
               child: Container(
                 alignment: Alignment.center,
-                margin: (index % 2) == 0
-                    ? EdgeInsets.only(right: 10, bottom: 10, top: 10)
-                    : EdgeInsets.only(left: 10, bottom: 10, top: 10),
                 padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
                 decoration: BoxDecoration(
                   border: Border.all(
@@ -226,12 +245,10 @@ class ManageRoomState extends State<ManageRoom> {
                 ),
                 child: Column(
                   children: [
-                    Expanded(
-                      child: Icon(
-                        Icons.room_preferences_outlined,
-                        color: Colors.white,
-                        size: 70,
-                      ),
+                    Icon(
+                      Icons.room_preferences_outlined,
+                      color: Colors.white,
+                      size: 70,
                     ),
                     SizedBox(
                       height: 10,
@@ -259,8 +276,13 @@ class ManageRoomState extends State<ManageRoom> {
             );
           },
           itemCount: 10,
-          gridDelegate:
-              SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+          gridDelegate: SliverStaggeredGridDelegateWithFixedCrossAxisCount(
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10,
+            crossAxisCount: 2,
+            staggeredTileCount: 10,
+            staggeredTileBuilder: (index) => StaggeredTile.fit(1),
+          ),
           shrinkWrap: true,
         ),
       ),
