e30272273d3b0fe90a601c7a5d2aa5127fcd5010
quifar <risqufaris@gmail.com>
2021-01-06 10:47:20 +0700

    create member_contract.dart

---
+++ lib/member_contract.dart
@@ -0,0 +1,11 @@
+import 'package:iot_starbhak_client/member_model.dart';
+
+class MemberContractView{
+  setOnSuccessMemberData(MemberModel memberModel) {}
+
+  setOnErrorMemberData(error) {}
+}
+class MemberContractPresenter{
+  getMemberData() {}
+  loadMemberData() {}
+}
