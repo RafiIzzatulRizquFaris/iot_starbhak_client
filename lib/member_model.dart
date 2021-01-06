19f077b8565786703f3a1db83af337e8de7eb41f
quifar <risqufaris@gmail.com>
2021-01-06 10:46:18 +0700

    create member_model.dart

---
+++ lib/member_model.dart
@@ -0,0 +1,67 @@
+class MemberModel {
+    String message;
+    List<Result> result;
+    bool success;
+
+    MemberModel({this.message, this.result, this.success});
+
+    factory MemberModel.fromJson(Map<String, dynamic> json) {
+        return MemberModel(
+            message: json['message'], 
+            result: json['result'] != null ? (json['result'] as List).map((i) => Result.fromJson(i)).toList() : null, 
+            success: json['success'], 
+        );
+    }
+
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        data['message'] = this.message;
+        data['success'] = this.success;
+        if (this.result != null) {
+            data['result'] = this.result.map((v) => v.toJson()).toList();
+        }
+        return data;
+    }
+}
+
+class Result {
+    int client_id;
+    Object created_at;
+    String email;
+    int id;
+    String member_token;
+    String password;
+    String role;
+    String updated_at;
+    String username;
+
+    Result({this.client_id, this.created_at, this.email, this.id, this.member_token, this.password, this.role, this.updated_at, this.username});
+
+    factory Result.fromJson(Map<String, dynamic> json) {
+        return Result(
+            client_id: json['client_id'], 
+            created_at: json['created_at'],
+            email: json['email'], 
+            id: json['id'], 
+            member_token: json['member_token'], 
+            password: json['password'], 
+            role: json['role'], 
+            updated_at: json['updated_at'],
+            username: json['username'], 
+        );
+    }
+
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        data['client_id'] = this.client_id;
+        data['email'] = this.email;
+        data['id'] = this.id;
+        data['member_token'] = this.member_token;
+        data['password'] = this.password;
+        data['role'] = this.role;
+        data['username'] = this.username;
+        data['created_at'] = this.created_at;
+        data['updated_at'] = this.updated_at;
+        return data;
+    }
+}
