f48bdd6ee3542e9ab495326ff0f3cdfa9e322f65
quifar <risqufaris@gmail.com>
2021-01-06 13:09:02 +0700

    update member_model.dart

--- lib/member_model.dart
+++ lib/member_model.dart
@@ -1,14 +1,16 @@
 class MemberModel {
     String message;
     List<Result> result;
+    Service service;
     bool success;
 
-    MemberModel({this.message, this.result, this.success});
+    MemberModel({this.message, this.result, this.service, this.success});
 
     factory MemberModel.fromJson(Map<String, dynamic> json) {
         return MemberModel(
             message: json['message'], 
             result: json['result'] != null ? (json['result'] as List).map((i) => Result.fromJson(i)).toList() : null, 
+            service: json['service'] != null ? Service.fromJson(json['service']) : null, 
             success: json['success'], 
         );
     }
@@ -20,6 +22,33 @@ class MemberModel {
         if (this.result != null) {
             data['result'] = this.result.map((v) => v.toJson()).toList();
         }
+        if (this.service != null) {
+            data['service'] = this.service.toJson();
+        }
+        return data;
+    }
+}
+
+class Service {
+    int id;
+    int price;
+    String type;
+
+    Service({this.id, this.price, this.type});
+
+    factory Service.fromJson(Map<String, dynamic> json) {
+        return Service(
+            id: json['id'], 
+            price: json['price'], 
+            type: json['type'], 
+        );
+    }
+
+    Map<String, dynamic> toJson() {
+        final Map<String, dynamic> data = new Map<String, dynamic>();
+        data['id'] = this.id;
+        data['price'] = this.price;
+        data['type'] = this.type;
         return data;
     }
 }
@@ -46,7 +75,7 @@ class Result {
             member_token: json['member_token'], 
             password: json['password'], 
             role: json['role'], 
-            updated_at: json['updated_at'],
+            updated_at: json['updated_at'], 
             username: json['username'], 
         );
     }
@@ -59,9 +88,9 @@ class Result {
         data['member_token'] = this.member_token;
         data['password'] = this.password;
         data['role'] = this.role;
+        data['updated_at'] = this.updated_at;
         data['username'] = this.username;
         data['created_at'] = this.created_at;
-        data['updated_at'] = this.updated_at;
         return data;
     }
 }
