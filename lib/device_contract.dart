4da15b594966763289bb7801481bc792ffdbb9da
quifar <risqufaris@gmail.com>
2021-01-07 09:26:27 +0700

    create device_contract.dart

---
+++ lib/device_contract.dart
@@ -0,0 +1,10 @@
+import 'device_model.dart';
+
+class DeviceContractView{
+  setOnSuccessDeviceData(DeviceModel deviceModel) {}
+  setOnErrorDeviceData(error) {}
+}
+class DeviceContractPresenter{
+  getDeviceData() {}
+  loadDeviceData() {}
+}
