e6220fd34870266e01e68bc5d1a5627c5e1b736d
quifar <risqufaris@gmail.com>
2021-01-07 09:26:09 +0700

    create get_usage_contract.dart

---
+++ lib/get_usage_contract.dart
@@ -0,0 +1,11 @@
+import 'get_usage_model.dart';
+
+class GetUsageContractView{
+  setOnSuccessGetUsageData(GetUsageModel getUsageModel) {}
+  setOnErrorGetUsageData(error) {}
+}
+
+class GetUsageContractPresenter{
+  getGetUsageData() {}
+  loadGetUsageData() {}
+}
