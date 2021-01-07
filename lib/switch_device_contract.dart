0870a7a54f1bfd6c3e82fc38b8491537d2c691e0
quifar <risqufaris@gmail.com>
2021-01-07 09:25:47 +0700

    create switch_device_contract.dart

---
+++ lib/switch_device_contract.dart
@@ -0,0 +1,8 @@
+class SwitchDeviceContractView{
+  setOnSuccessSwitchDeviceData(int statusCode) {}
+  setOnErrorSwitchDeviceData(error) {}
+}
+class SwitchDeviceContractPresenter{
+  getSwitchDeviceData(String deviceId, String areaId) {}
+  loadSwitchDeviceData(String deviceId, String areaId) {}
+}
