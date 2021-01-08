a9af88e861a7c809c7213ee4ba90f91d40922743
quifar <risqufaris@gmail.com>
2021-01-08 14:39:49 +0700

    update info.dart : add navigation to contact.dart

--- lib/info.dart
+++ lib/info.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:iot_starbhak_client/contact.dart';
 import 'package:iot_starbhak_client/member_contract.dart';
 import 'package:iot_starbhak_client/member_model.dart';
 import 'package:iot_starbhak_client/member_presenter.dart';
+import 'package:page_transition/page_transition.dart';
 import 'string_extension.dart';
 
 class Info extends StatefulWidget {
@@ -13,14 +15,13 @@ class Info extends StatefulWidget {
   }
 }
 
-class InfoState extends State<Info> implements MemberContractView{
-
+class InfoState extends State<Info> implements MemberContractView {
   MemberPresenter memberPresenter;
   bool isLoading = false;
   String placeName = "Place Name";
   String membership = "Membership";
 
-  InfoState(){
+  InfoState() {
     memberPresenter = MemberPresenter(this);
   }
 
@@ -70,137 +71,159 @@ class InfoState extends State<Info> implements MemberContractView{
                 );
               },
             ),
+            actions: [
+              IconButton(
+                icon: Icon(
+                  Icons.info_outline_rounded,
+                  color: Colors.blue,
+                  size: 30,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    PageTransition(
+                      child: ContactScreen(),
+                      type: PageTransitionType.fade,
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
         ),
       ),
-      body: isLoading ? Center(
-        child: CircularProgressIndicator(
-          backgroundColor: Colors.blue,
-        ),
-      ) : Container(
-        width: MediaQuery.of(context).size.width,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              child: Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.all(20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.70,
-                      padding: EdgeInsets.all(20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.blue,
-                      ),
+      body: isLoading
+          ? Center(
+              child: CircularProgressIndicator(
+                backgroundColor: Colors.blue,
+              ),
+            )
+          : Container(
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Container(
+                      alignment: Alignment.center,
+                      margin: EdgeInsets.all(20),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.max,
                         children: [
                           Container(
+                            width: MediaQuery.of(context).size.width * 0.70,
+                            padding: EdgeInsets.all(20),
                             decoration: BoxDecoration(
-                              shape: BoxShape.circle,
-                              color: Colors.blue.shade400,
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.blue,
                             ),
-                            padding: EdgeInsets.all(10),
-                            child: Icon(
-                              Icons.home,
-                              color: Colors.white,
-                              size: 70,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 12,
-                          ),
-                          Text(
-                            "$placeName's Home",
-                            textAlign: TextAlign.center,
-                            maxLines: 2,
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 8,
-                          ),
-                          Text(
-                            "Plan : ",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.white.withAlpha(200),
-                              fontSize: 18,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text(
-                            membership,
-                            textAlign: TextAlign.center,
-                            style: TextStyle(
-                              color: Colors.amber,
-                              fontSize: 18,
-                            ),
-                          ),
-                          SizedBox(
-                            height: 12,
-                          ),
-                          Container(
-                            width: MediaQuery.of(context).size.width * 0.60,
-                            child: ElevatedButton(
-                              style: ButtonStyle(
-                                padding: MaterialStateProperty.resolveWith(
-                                    (states) => EdgeInsets.all(10)),
-                                shape: MaterialStateProperty.resolveWith(
-                                    (states) => RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(5),
-                                        )),
-                                backgroundColor:
-                                    MaterialStateProperty.resolveWith(
-                                        (states) => Colors.white),
-                                side: MaterialStateProperty.resolveWith(
-                                  (states) => BorderSide(
-                                    color: Colors.blue,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Container(
+                                  decoration: BoxDecoration(
+                                    shape: BoxShape.circle,
+                                    color: Colors.blue.shade400,
+                                  ),
+                                  padding: EdgeInsets.all(10),
+                                  child: Icon(
+                                    Icons.home,
+                                    color: Colors.white,
+                                    size: 70,
                                   ),
                                 ),
-                              ),
-                              child: Text(
-                                "Upgrade",
-                                style: TextStyle(
-                                  color: Colors.blue,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20,
+                                SizedBox(
+                                  height: 12,
                                 ),
-                              ),
-                              onPressed: () {},
+                                Text(
+                                  "$placeName's Home",
+                                  textAlign: TextAlign.center,
+                                  maxLines: 2,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 8,
+                                ),
+                                Text(
+                                  "Plan : ",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.white.withAlpha(200),
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                Text(
+                                  membership,
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.amber,
+                                    fontSize: 18,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 12,
+                                ),
+                                Container(
+                                  width:
+                                      MediaQuery.of(context).size.width * 0.60,
+                                  child: ElevatedButton(
+                                    style: ButtonStyle(
+                                      padding:
+                                          MaterialStateProperty.resolveWith(
+                                              (states) => EdgeInsets.all(10)),
+                                      shape: MaterialStateProperty.resolveWith(
+                                          (states) => RoundedRectangleBorder(
+                                                borderRadius:
+                                                    BorderRadius.circular(5),
+                                              )),
+                                      backgroundColor:
+                                          MaterialStateProperty.resolveWith(
+                                              (states) => Colors.white),
+                                      side: MaterialStateProperty.resolveWith(
+                                        (states) => BorderSide(
+                                          color: Colors.blue,
+                                        ),
+                                      ),
+                                    ),
+                                    child: Text(
+                                      "Upgrade",
+                                      style: TextStyle(
+                                        color: Colors.blue,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20,
+                                      ),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              ],
                             ),
-                          ),
+                          )
                         ],
                       ),
-                    )
-                  ],
-                ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20),
+                    child: Text(
+                      "StarIOT 2020",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
-            Container(
-              padding: EdgeInsets.all(20),
-              child: Text(
-                "StarIOT 2020",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
     );
   }
 
@@ -211,7 +234,7 @@ class InfoState extends State<Info> implements MemberContractView{
 
   @override
   setOnSuccessMemberData(MemberModel memberModel) {
-    if (memberModel.success){
+    if (memberModel.success) {
       setState(() {
         placeName = memberModel.message.replaceAll("members", "").trim();
         membership = memberModel.service.type.capitalize();
