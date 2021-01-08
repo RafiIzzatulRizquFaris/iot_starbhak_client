bb50f88fd2e7dde7e3b3a238b9fd3cf22bd4ffe3
quifar <risqufaris@gmail.com>
2021-01-08 14:38:41 +0700

    create history_contract.dart

---
+++ lib/history_contract.dart
@@ -0,0 +1,11 @@
+import 'history_model.dart';
+
+class HistoryContractView{
+  setOnSuccessHistoryData(HistoryModel historyModel) {}
+
+  setOnErrorHistoryData(error) {}
+}
+class HistoryContractPresenter{
+  getHistoryData() {}
+  loadHistoryData() {}
+}
